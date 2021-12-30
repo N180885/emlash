3dcac8339490c3069e8fa2ce5c0516958c93d16c
N180885 <teskarisiparol@gmail.com>
2021-12-30 21:33:47 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./style.css";
-import Nav from "./components/nav";
-import Main from "./components/main";
-import Home from "./components/home";
-import Enter from "./views/enter";
-import Register from "./views/register";
-import Sot from "./views/sot";
-import Rez from "./views/rez";
-import Lech from "./views/lech";
-import Voz from "./img/voz.jpeg"
-import Info from "./views/info";
-import Add from "./views/add";
-import List from "./views/list";
+import Home1 from "./components/home1";
+
 
 const App = () => {
   return (
     <>
-      <img src={Voz} alt=""/>      
-      <Nav />
-      
-      <BrowserRouter>
-      <Switch>      
-          <Route exact path="/" component={Home} />;
-          <Route exact path="/main" component={Main} />;
-          <Route exact path="/register" component={Register} />;
-          <Route exact path="/enter" component={Enter} />;
-          <Route exact path="/sot" component={Sot} />;
-          <Route exact path="/rez" component={Rez} />;
-          <Route exact path="/lech" component={Lech} />;
-          <Route exact path="/info" component={Info} />;
-          <Route exact path="/add" component={Add} />;
-          <Route exact path="/list" component={List} />;
-        </Switch>
-        </BrowserRouter>        
+     <Home1/>      
     </>
   );
 };
---
+++ src/components/home1.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Voz from "../img/voz.jpeg"
+import Nav from "./nav";
+import Main from "./main";
+import Home from "./home";
+import Enter from "../views/enter";
+import Register from "../views/register";
+import Sot from "../views/sot";
+import Rez from "../views/rez";
+import Lech from "../views/lech";
+import Info from "../views/info";
+import Add from "../views/add";
+import List from "../views/list";
+
+class Home1 extends Component {
+  state = {
+   
+  };
+  render() {
+    return (
+      <>      
+      <img src={Voz} alt=""/>   
+      <Nav/>
+      <BrowserRouter>
+        <Switch>
+        <Route exact path="/" component={Home} />;
+        <Route exact path="/main" component={Main} />;
+        <Route exact path="/register" component={Register} />;
+        <Route exact path="/enter" component={Enter} />;
+        <Route exact path="/sot" component={Sot} />;
+        <Route exact path="/rez" component={Rez} />;
+        <Route exact path="/lech" component={Lech} />;
+        <Route exact path="/info" component={Info} />;
+        <Route exact path="/add" component={Add} />;
+        <Route exact path="/list" component={List} />;
+        </Switch>
+        </BrowserRouter>
+      </>
+    );
+  }
+}
+export default Home1;
